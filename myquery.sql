b3f4b371d23e8b328128cf2bfab6f6593e87adf9
Aasritha29 <aasritha.grandhi@gmail.com>
2023-07-14 07:21:34 +0000

    dropped table myuser

---
+++ myquery.sql
@@ -0,0 +1,7 @@
+create table myuser(name varchar2(30));
+desc myuser;
+insert into myuser(name) values('iamneo');
+select * from myuser;
+insert into myuser(name) values('neo');
+select * from myuser;
+drop table myuser;
