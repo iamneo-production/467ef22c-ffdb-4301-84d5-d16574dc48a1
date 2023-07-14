a359fcf27a268304424f86ca85d7e00d4c32859d
Aasritha29 <aasritha.grandhi@gmail.com>
2023-07-14 17:10:16 +0000

    Commit Completed

---
+++ Airlines(aasritha).sql
@@ -0,0 +1,31 @@
+set timing on;
+select * from airline;
+
+---First Query
+
+SELECT COUNT(S_ID) AS Female_Passengers_count FROM 
+airline WHERE GENDER LIKE 'Female' ;
+
+
+--Second Query
+
+SELECT COUNT(S_ID) AS Business_class_count FROM
+airline WHERE CLASS='Business';
+
+
+--Third Query
+
+SELECT COUNT(S_ID) AS Eco_class_count FROM
+airline WHERE CLASS='Eco';
+
+
+--Fourth Query
+
+SELECT COUNT(S_ID) AS Children_count FROM
+airline WHERE  age<13;
+
+
+--Fifth Query
+
+ SELECT COUNT(S_ID) AS Business_class_count_above16 FROM 
+AIRLINE WHERE age>16 and CLASS='Business';   
