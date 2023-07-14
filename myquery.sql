9622f709ab0f389b4501188de37a21eaee27871d
Aasritha29 <102411305+Aasritha29@users.noreply.github.com>
2023-07-14 13:25:00 +0530

    Update myquery.sql

--- myquery.sql
+++ myquery.sql
@@ -1,7 +1,1 @@
-create table myuser(name varchar2(30));
-desc myuser;
-insert into myuser(name) values('iamneo');
-select * from myuser;
-insert into myuser(name) values('neo');
-select * from myuser;
-drop table myuser;
+
