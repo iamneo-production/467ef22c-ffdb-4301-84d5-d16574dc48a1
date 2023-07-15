24b14fd3ddc56153f46e7805dcb543059bb121c5
Aasritha29 <aasritha.grandhi@gmail.com>
2023-07-15 12:51:35 +0000

    Commit completed

--- Airlines(aasritha).sql
+++ Airlines(aasritha).sql
@@ -3,32 +3,32 @@ select * from airline;
 
 ---First Query
 
-SELECT COUNT(S_ID) AS Female_Passengers_count FROM 
-airline WHERE GENDER LIKE 'Female' ;
+SELECT COUNT(GENDER) AS Female_Passengers_count FROM 
+airline WHERE GENDER='Female' ;
+
 
 --Second Query
 
-SELECT COUNT(S_ID) AS Business_class_count FROM
+SELECT COUNT(CLASS) AS Business_class_count FROM
 airline WHERE CLASS='Business';
 
 
 --Third Query
 
-SELECT COUNT(S_ID) AS Eco_class_count FROM
+SELECT COUNT(CLASS) AS Eco_class_count FROM
 airline WHERE CLASS='Eco';
 
 
 --Fourth Query
 
-SELECT COUNT(S_ID) AS Children_count FROM
+SELECT COUNT(AGE) AS Children_count FROM
 airline WHERE  age<13;
 
 
 --Fifth Query
 
- SELECT COUNT(S_ID) AS Business_class_count_above16 FROM 
+ SELECT COUNT(CLASS) AS Business_class_count_above16 FROM 
 AIRLINE WHERE age>16 and CLASS='Business';   
-
 
 
 
