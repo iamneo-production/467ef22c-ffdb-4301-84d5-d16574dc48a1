dfcfc5c595f19454d0364ae34dd95176313a471c
SADHUUDAYKIRAN <amma20795@gmail.com>
2023-07-24 10:49:48 +0000

    successfully committed

---
+++ uday kiran.sql
@@ -0,0 +1,28 @@
+SELECT COUNT(*) AS female_passenger_count
+FROM AIRLINE
+WHERE GENDER = 'Female';
+/
+
+
+SELECT COUNT(*) AS business_class_passenger_count
+FROM AIRLINE
+WHERE CLASS = 'Business';
+/
+
+
+SELECT COUNT(*) AS economy_class_passenger_count
+FROM AIRLINE
+WHERE CLASS = 'Eco';
+/
+
+
+SELECT COUNT(*) AS child_passenger_count
+FROM AIRLINE
+WHERE AGE < 13;
+/
+
+
+SELECT COUNT(*) AS passengers_above_16_in_business_class_count
+FROM AIRLINE
+WHERE AGE > 16 AND CLASS = 'Business';
+/
