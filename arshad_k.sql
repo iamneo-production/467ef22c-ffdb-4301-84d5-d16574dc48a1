acf36419649d1cf1bf186926f7f48407dd04ab7c
Arshad272 <arshadkareem272@gmail.com>
2023-07-28 11:06:44 +0000

    Changed file name and added my queries

---
+++ arshad_k.sql
@@ -0,0 +1,31 @@
+--My Queries
+--First Query
+SELECT COUNT(*) AS female_passenger_count FROM AIRLINE WHERE GENDER = 'Female';
+
+--Second Query
+SELECT COUNT(*) AS business_class_passenger_count FROM AIRLINE WHERE CLASS = 'Business';
+
+--Third Query
+SELECT COUNT(*) AS economy_class_passenger_count FROM AIRLINE WHERE CLASS = 'Eco';
+
+--Fourth Query
+SELECT COUNT(*) AS child_passenger_count FROM AIRLINE WHERE AGE < 13;
+
+--Fifth Query
+SELECT COUNT(*) AS passengers_above_16_in_business_class_count FROM AIRLINE WHERE AGE > 16 AND CLASS = 'Business';
+
+--Optimised Queries
+--First Query
+SELECT COUNT(*) AS female_passenger_count FROM AIRLINE WHERE GENDER = 'Female';
+
+--Second Query
+SELECT COUNT(*) AS business_class_passenger_count FROM AIRLINE WHERE CLASS = 'Business';
+
+--Third Query
+SELECT COUNT(*) AS economy_class_passenger_count FROM AIRLINE WHERE CLASS = 'Eco';
+
+--Fourth Query
+SELECT COUNT(*) AS child_passenger_count FROM AIRLINE WHERE AGE < 13;
+
+--Fifth Query
+SELECT COUNT(*) AS passengers_above_16_in_business_class_count FROM AIRLINE WHERE AGE > 16 AND CLASS = 'Business';
