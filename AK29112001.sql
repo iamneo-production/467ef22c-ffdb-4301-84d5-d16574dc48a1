ee891ce7893895f8e1d6ccf2bc0e9f75a9d96771
AK29112001 <akshaystormfire1@gmail.com>
2023-07-29 11:11:53 +0000

    c

--- AK29112001.sql
+++ AK29112001.sql
@@ -16,7 +16,6 @@ SELECT COUNT(*) AS female_passenger_count FROM AIRLINE WHERE GENDER = 'Female';
 SELECT COUNT(*) AS business_class_passenger_count FROM AIRLINE WHERE CLASS = 'Business';
 
 /* 3. query to find number of economy class passengers*/
-
 SELECT COUNT(*) AS economy_class_passenger_count FROM AIRLINE WHERE CLASS = 'Eco';
 SELECT COUNT(*) AS child_passenger_count FROM AIRLINE WHERE AGE < 13;
 SELECT COUNT(*) AS passengers_above_16_in_business_class_count FROM AIRLINE WHERE AGE > 16 AND CLASS = 'Business';
