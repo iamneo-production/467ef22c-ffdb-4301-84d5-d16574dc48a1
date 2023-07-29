48123b05855b4a279143374820fd01427dcb746b
AK29112001 <akshaystormfire1@gmail.com>
2023-07-29 11:02:51 +0000

    commit

--- AK29112001.sql
+++ AK29112001.sql
@@ -14,6 +14,9 @@ SELECT COUNT(*) AS female_passenger_count FROM AIRLINE WHERE GENDER = 'Female';
 
 /* 2. query to find the total number of passengers who have made bookings for air tickets */
 SELECT COUNT(*) AS business_class_passenger_count FROM AIRLINE WHERE CLASS = 'Business';
+
+/* 3. query to find number of economy class passengers*/
+
 SELECT COUNT(*) AS economy_class_passenger_count FROM AIRLINE WHERE CLASS = 'Eco';
 SELECT COUNT(*) AS child_passenger_count FROM AIRLINE WHERE AGE < 13;
 SELECT COUNT(*) AS passengers_above_16_in_business_class_count FROM AIRLINE WHERE AGE > 16 AND CLASS = 'Business';
