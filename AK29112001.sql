0fc3e22350607fe8582d43f65ae15dd0905b838c
AK29112001 <akshaystormfire1@gmail.com>
2023-07-29 11:35:15 +0000

    C

--- AK29112001.sql
+++ AK29112001.sql
@@ -1,21 +1,27 @@
 
 SELECT  
-    COUNT(CASE WHEN GENDER = 'Female' THEN 1 END) FEMALE_PASSENGERS, /* 1. count of female passengers */
-    COUNT(CASE WHEN CLASS = 'Business' THEN 1 END) BUSINESS_COUNT, /*2. count of business class passengers */
-    COUNT(CASE WHEN CLASS = 'Eco' THEN 1 END) ECONOMY_COUNT, /* 3. count of economy class passengers */
-    COUNT(CASE WHEN AGE < 13 THEN 1 END) CHILDREN_BELOW_13, /* 4. count of children with age below 13 */
-    COUNT(CASE WHEN AGE > 16 AND CLASS = 'Business' THEN 1 END) BUSINESS_WITH_AGE_ABOVE_16 /*5. count of business class passengers with age above 16*/
+    COUNT(CASE WHEN GENDER = 'Female' THEN 1 END) AS FEMALE_PASSENGERS, 
+    COUNT(CASE WHEN CLASS = 'Business' THEN 1 END) AS BUSINESS_COUNT, 
+    COUNT(CASE WHEN CLASS = 'Eco' THEN 1 END) AS ECONOMY_COUNT,
+    COUNT(CASE WHEN AGE < 13 THEN 1 END) AS CHILDREN_BELOW_13, 
+    COUNT(CASE WHEN AGE > 16 AND CLASS = 'Business' THEN 1 END) AS BUSINESS_WITH_AGE_ABOVE_16 
 FROM    
     airline;
 
+
 --OPTIMIZED QUERIES
-/* 1. query to find the number of female passengers who have made bookings for air tickets */
+
+-- 1. query to find the number of female passengers who have made bookings for air tickets
 SELECT COUNT(*) AS female_passenger_count FROM AIRLINE WHERE GENDER = 'Female';
 
-/* 2. query to find the total number of passengers who have made bookings for air tickets */
+-- 2. query to find the total number of passengers who have made bookings for air tickets
 SELECT COUNT(*) AS business_class_passenger_count FROM AIRLINE WHERE CLASS = 'Business';
 
-/* 3. query to find number of economy class passengers*/
+-- 3. query to find number of economy class passengers
 SELECT COUNT(*) AS economy_class_passenger_count FROM AIRLINE WHERE CLASS = 'Eco';
+
+-- 4. query to find the number of children who are all booked tickets
 SELECT COUNT(*) AS child_passenger_count FROM AIRLINE WHERE AGE < 13;
+
+-- 5. query to find count of passengers who are above the age of 16 and have booked tickets in the business class
 SELECT COUNT(*) AS passengers_above_16_in_business_class_count FROM AIRLINE WHERE AGE > 16 AND CLASS = 'Business';
