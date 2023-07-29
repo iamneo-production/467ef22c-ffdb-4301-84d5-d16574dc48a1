c1bcb99fded5c115f6c0b8ce26ae7aa4b10faa10
AK29112001 <113778960+AK29112001@users.noreply.github.com>
2023-07-29 18:41:37 +0530

    Update AK29112001.sql

--- AK29112001.sql
+++ AK29112001.sql
@@ -1,25 +1,15 @@
 set timing on;
 
 --MY QUERIES
--- 1. query to find the number of female passengers who have made bookings for air tickets
+
 SELECT  
-    COUNT(CASE WHEN GENDER = 'Female' THEN 1 END) AS FEMALE_PASSENGERS FROM airline ;
-
--- 2. query to find the total number of passengers who have made bookings for air tickets
-SELECT
-    COUNT(CASE WHEN CLASS = 'Business' THEN 1 END) AS BUSINESS_CLASS_COUNT FROM airline;
-
--- 3. query to find number of economy class passengers
-SELECT
-    COUNT(CASE WHEN CLASS = 'Eco' THEN 1 END) AS ECONOMY_CLASS_COUNT FROM airline;
-
--- 4. query to find the number of children who are all booked tickets
-SELECT
-    COUNT(CASE WHEN AGE < 13 THEN 1 END) AS CHILDREN_COUNT FROM airline;
-
--- 5. query to find count of passengers who are above the age of 16 and have booked tickets in the business class
-SELECT
-    COUNT(CASE WHEN AGE > 16 AND CLASS = 'Business' THEN 1 END) AS BUSINESS_CLASS_PASSENGERS_ABOVE_16 FROM airline;
+    COUNT(CASE WHEN GENDER = 'Female' THEN 1 END) AS FEMALE_PASSENGERS, 
+    COUNT(CASE WHEN CLASS = 'Business' THEN 1 END) AS BUSINESS_CLASS_COUNT, 
+    COUNT(CASE WHEN CLASS = 'Eco' THEN 1 END) AS ECONOMY_CLASS_COUNT,
+    COUNT(CASE WHEN AGE < 13 THEN 1 END) AS CHILDREN_COUNT, 
+    COUNT(CASE WHEN AGE > 16 AND CLASS = 'Business' THEN 1 END) AS BUSINESS_CLASS_PASSENGERS_ABOVE_16 
+FROM    
+    airline;
 
 
 --OPTIMIZED QUERIES
