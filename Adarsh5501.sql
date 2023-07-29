4ba23e84411c4c34d8e0187b2ff856994695478d
Adarsh5501 <adarshdubey69@gmail.com>
2023-07-29 14:21:01 +0000

    commit

--- Adarsh5501.sql
+++ Adarsh5501.sql
@@ -1,16 +1,45 @@
+--My Queries
+
+--First Query
 SELECT COUNT(*) AS FEMALE_PASSENGER_COUNT FROM AIRLINE
 WHERE GENDER = 'Female';
 
+--Second Query
 SELECT COUNT(*) AS BUSINESS_CLASS_PASSENGER_COUNT FROM AIRLINE
 WHERE CLASS = 'Business';
 
+--Third Query
 SELECT COUNT(*) AS ECO_CLASS_PASSENGER_COUNT FROM AIRLINE
 WHERE CLASS = 'Eco';
 
+--Fourth Query
 SELECT COUNT(*) AS AGE_BELOW_13_PASSENGER_COUNT FROM AIRLINE
 WHERE AGE < 13;
 
+--Fifth Query
 SELECT COUNT(*) AS AGE_ABOVE_16_BUSINESS_CLASS_PASSENGER_COUNT FROM(
     SELECT ID FROM AIRLINE
     WHERE AGE > 16 AND CLASS = 'Business'
-) AS SUB;
+) SUB;
+
+--Optimized Final Queries
+
+---First Query
+SELECT COUNT(*) AS female_passenger_count
+ FROM AIRLINE WHERE GENDER = 'Female';
+
+ --Second Query
+SELECT COUNT(*) AS business_class_passenger_count
+ FROM AIRLINE WHERE CLASS ='Business';
+
+ --Third Query
+SELECT COUNT(*) AS economy_class_passenger_count 
+FROM AIRLINE WHERE CLASS = 'Eco';
+
+--Fourth Query
+SELECT COUNT(*) AS child_passenger_count 
+FROM AIRLINE WHERE AGE < 13;
+
+--Fifth Query
+SELECT COUNT(*)  as Business_class_count_above_16 from airline where 
+age>16 and class='Business';
