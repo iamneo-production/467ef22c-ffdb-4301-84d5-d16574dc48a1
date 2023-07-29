8fc915cd6cf210cab6c24bd2049fca77333df539
Aishu28-10 <aishukannan28@gmail.com>
2023-07-29 09:14:20 +0000

    commit

---
+++ Aishwarya_K.sql
@@ -0,0 +1,7 @@
+SELECT 
+SUM(CASE WHEN gender = 'Female' THEN 1 ELSE 0 END) AS Female_Passengers_Count, --1st query
+SUM(CASE WHEN Class = 'Business' THEN 1 ELSE 0 END) AS BusinessClass_Passengers_Count,  --2nd query
+SUM(CASE WHEN Class = 'Eco' THEN 1 ELSE 0 END) AS EconomyClass_Passengers_Count,  
+SUM(CASE WHEN age < 13 THEN 1 ELSE 0 END) AS Children_Passengers_Count,
+SUM(CASE WHEN age > 16 AND Class = 'Business' THEN 1 ELSE 0 END) AS AdultBusinessClass_Passengers_Count
+FROM airline;
