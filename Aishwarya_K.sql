6fc44bebf8f8bf9a7e2561e60d91fe7daace233d
Aishu28-10 <aishukannan28@gmail.com>
2023-07-29 12:28:40 +0000

    s

--- Aishwarya_K.sql
+++ Aishwarya_K.sql
@@ -13,10 +13,25 @@ SUM(CASE WHEN age > 16 AND Class = 'Business' THEN 1 ELSE 0 END) AS AdultBusines
 FROM airline;
 
 
---OPTIMIZED QUERY
+--OPTIMIZED QUERIES
+--Final
 
-SELECT COUNT(*) AS female_passenger_count FROM AIRLINE WHERE GENDER = 'Female'; 
-SELECT COUNT(*) AS business_class_passenger_count FROM AIRLINE WHERE CLASS = 'Business';
-SELECT COUNT(*) AS economy_class_passenger_count FROM AIRLINE WHERE CLASS = 'Eco';
-SELECT COUNT(*) AS child_passenger_count FROM AIRLINE WHERE AGE < 13;
-SELECT COUNT(*) AS passengers_above_16_in_business_class_count FROM AIRLINE WHERE AGE > 16 AND CLASS = 'Business';
+---First Query
+SELECT COUNT(*) AS female_passenger_count
+ FROM AIRLINE WHERE GENDER = 'Female';
+
+ --Second Query
+SELECT COUNT(*) AS business_class_passenger_count
+ FROM AIRLINE WHERE CLASS = 'Business';
+
+ --Third Query
+SELECT COUNT(*) AS economy_class_passenger_count 
+FROM AIRLINE WHERE CLASS = 'Eco';
+
+--Fourth Query
+SELECT COUNT(*) AS child_passenger_count 
+FROM AIRLINE WHERE AGE < 13;
+
+--Fifth Query
+SELECT COUNT(*)  as Business_class_count_above_16 from airline where 
+age>16 and class='Business'; 
