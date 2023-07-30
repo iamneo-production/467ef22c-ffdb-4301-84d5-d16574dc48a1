9bb962f1fe9602ff1941b7559103547542e2b305
AK29112001 <113778960+AK29112001@users.noreply.github.com>
2023-07-30 10:14:57 +0530

    Update AK29112001.sql

--- AK29112001.sql
+++ AK29112001.sql
@@ -1,13 +1,16 @@
 set timing on;
 
+VARIABLE myConstant VARCHAR2(10);
+EXEC :myConstant := 'Business';
+
 --MY QUERIES
 
 SELECT  
     COUNT(CASE WHEN GENDER = 'Female' THEN 1 END) AS FEMALE_PASSENGERS, 
-    COUNT(CASE WHEN CLASS = 'Business' THEN 1 END) AS BUSINESS_CLASS_COUNT, 
+    COUNT(CASE WHEN CLASS = :myConstant THEN 1 END) AS BUSINESS_CLASS_COUNT, 
     COUNT(CASE WHEN CLASS = 'Eco' THEN 1 END) AS ECONOMY_CLASS_COUNT,
     COUNT(CASE WHEN AGE < 13 THEN 1 END) AS CHILDREN_COUNT, 
-    COUNT(CASE WHEN AGE > 16 AND CLASS = 'Business' THEN 1 END) AS BUSINESS_CLASS_PASSENGERS_ABOVE_16 
+    COUNT(CASE WHEN AGE > 16 AND CLASS = :myConstant THEN 1 END) AS BUSINESS_CLASS_PASSENGERS_ABOVE_16 
 FROM    
     airline;
 
@@ -18,7 +21,7 @@ FROM
 SELECT COUNT(S_ID) AS female_passenger_count FROM AIRLINE WHERE GENDER = 'Female';
 
 -- 2. query to find the total number of passengers who have made bookings for air tickets
-SELECT COUNT(S_ID) AS business_class_passenger_count FROM AIRLINE WHERE CLASS = 'Business';
+SELECT COUNT(S_ID) AS business_class_passenger_count FROM AIRLINE WHERE CLASS = :myConstant;
 
 -- 3. query to find number of economy class passengers
 SELECT COUNT(S_ID) AS economy_class_passenger_count FROM AIRLINE WHERE CLASS = 'Eco';
@@ -27,4 +30,4 @@ SELECT COUNT(S_ID) AS economy_class_passenger_count FROM AIRLINE WHERE CLASS = '
 SELECT COUNT(S_ID) AS child_passenger_count FROM AIRLINE WHERE AGE < 13;
 
 -- 5. query to find count of passengers who are above the age of 16 and have booked tickets in the business class
-SELECT COUNT(S_ID) AS passengers_above_16_in_business_class_count FROM AIRLINE WHERE AGE > 16 AND CLASS = 'Business';
+SELECT COUNT(S_ID) AS passengers_above_16_in_business_class_count FROM AIRLINE WHERE AGE > 16 AND CLASS = :myConstant;
