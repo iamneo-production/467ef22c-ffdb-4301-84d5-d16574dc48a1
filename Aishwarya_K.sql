0f5ed7ccfefac13cc7e3ed542e60e39799bd83f7
Aishu28-10 <110662568+Aishu28-10@users.noreply.github.com>
2023-07-30 10:28:28 +0530

    Update Aishwarya_K.sql

--- Aishwarya_K.sql
+++ Aishwarya_K.sql
@@ -1,10 +1,13 @@
+VARIABLE myConstant VARCHAR2(10);
+EXEC :myConstant := 'Business';
+
 set timing on;
 SELECT 
 ---1st query
   COUNT(CASE WHEN gender = 'Female' THEN 1 END) AS Female_Passengers_Count,
 
 ---2nd query
-  COUNT(CASE WHEN Class = 'Business' THEN 1 END) AS BusinessClass_Passengers_Count,  
+  COUNT(CASE WHEN Class = :myconstant THEN 1 END) AS BusinessClass_Passengers_Count,  
 
 ---3rd query
   COUNT(CASE WHEN Class = 'Eco' THEN 1 END) AS EconomyClass_Passengers_Count,
@@ -13,7 +16,7 @@ SELECT
   COUNT(CASE WHEN age < 13 THEN 1 END) AS Children_Passengers_Count,
   
 ---5th query
-  COUNT(CASE WHEN age > 16 AND Class = 'Business' THEN 1 END) AS AdultBusinessClass_Passengers_Count
+  COUNT(CASE WHEN age > 16 AND Class = :myconstant THEN 1 END) AS AdultBusinessClass_Passengers_Count
 FROM airline;
 
 
@@ -26,7 +29,7 @@ SELECT COUNT(*) AS female_passenger_count
 
  --Second Query
 SELECT COUNT(*) AS business_class_passenger_count
- FROM AIRLINE WHERE CLASS = 'Business';
+ FROM AIRLINE WHERE CLASS = :myconstant;
 
  --Third Query
 SELECT COUNT(*) AS economy_class_passenger_count 
@@ -38,4 +41,4 @@ FROM AIRLINE WHERE AGE < 13;
 
 --Fifth Query
 SELECT COUNT(*)  as Business_class_count_above_16 from airline where 
-age>16 and class='Business'; 
+age>16 and class= :myconstant; 
