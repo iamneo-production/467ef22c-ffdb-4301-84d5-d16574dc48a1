88b2dc77518220c8f3691526852411f40232a614
SADHUUDAYKIRAN <132553769+SADHUUDAYKIRAN@users.noreply.github.com>
2023-07-30 10:46:49 +0530

    Update SADHUUDAYKIRAN.sql

--- udaykiran_sadhu.sql
+++ udaykiran_sadhu.sql
@@ -1,28 +1,31 @@
---Query 1
-SELECT COUNT(*) AS female_passengers_count
-FROM AIRLINE
-WHERE GENDER = 'Female';
+--My Queries
+set timing on;
+VARIABLE business VARCHAR2(10);
+EXEC :business := 'Business';
+--First Query
+SELECT COUNT(*) AS female_passenger_count FROM AIRLINE WHERE GENDER = 'Female';
+--Second Query
+SELECT COUNT(*) AS business_class_passenger_count FROM AIRLINE WHERE CLASS = :business;
+--Third Query
+SELECT COUNT(*) AS economy_class_passenger_count FROM AIRLINE WHERE CLASS = 'Eco';
+--Fourth Query
+SELECT COUNT(*) AS child_passenger_count FROM AIRLINE WHERE AGE < 13;
+--Fifth Query
+SELECT COUNT(*) AS passengers_above_16_in_business_class_count FROM AIRLINE WHERE AGE > 16 AND CLASS = :business;
 
 
---Query 2
-SELECT COUNT(*) AS business_class_passengers_count
-FROM AIRLINE
-WHERE CLASS = 'Business';
+--Optimised Queries 
+--First Query
+SELECT COUNT(*) AS female_passenger_count FROM AIRLINE WHERE GENDER = 'Female';
 
+--Second Query
+SELECT COUNT(*) AS business_class_passenger_count FROM AIRLINE WHERE CLASS = :business;
 
---Query 3
-SELECT COUNT(*) AS economy_class_passengers_count
-FROM AIRLINE
-WHERE CLASS = 'Eco';
+--Third Query
+SELECT COUNT(*) AS economy_class_passenger_count FROM AIRLINE WHERE CLASS = 'Eco';
 
+--Fourth Query
+SELECT COUNT(*) AS child_passenger_count FROM AIRLINE WHERE AGE < 13;
 
---Query 4
-SELECT COUNT(*) AS child_passengers_count
-FROM AIRLINE
-WHERE AGE < 13;
-
-
---Query 5
-SELECT COUNT(*) AS passengers_above_16_in_business_class_count
-FROM AIRLINE
-WHERE AGE > 16 AND CLASS = 'Business';
+--Fifth Query
+SELECT COUNT(*) AS passengers_above_16_in_business_class_count FROM AIRLINE WHERE AGE > 16 AND CLASS = :business;
